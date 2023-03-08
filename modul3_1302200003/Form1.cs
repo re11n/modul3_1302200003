6381beea9d4f0434f8908c3f95c3f80978c11707
re11n <ryansiahaan@student.telkomuniversity.ac.id>
2023-03-08 16:24:02 +0700

    menambahkan implementasi kalkulator

--- modul3_1302200003/Form1.cs
+++ modul3_1302200003/Form1.cs
@@ -12,9 +12,85 @@ namespace modul3_1302200003
 {
     public partial class Form1 : Form
     {
+        private int bil1;
+        private int bil2;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bil1 = int.Parse(label1.Text);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            label1.Text = "1";
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label1.Text = "0";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            label1.Text = "2";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            label1.Text = "3";
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            label1.Text = "4";
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            label1.Text = "5";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            label1.Text = "6";
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            label1.Text = "7";
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            label1.Text = "8";
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            label1.Text = "9";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            bil2 = int.Parse(label1.Text);
+            var hasil = bil1 + bil2;
+            label1.Text = "" + hasil;
         }
     }
 }
